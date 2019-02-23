41b605006ce23614c2f73cdf340066c8069f1bc7
Daniel Victor <daniel.victor9@hotmail.com>
2019-02-22 21:59:00 -0300

    preparação para início das atividades

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 class="title"> ❄️🤓 <br/> Daniel Victor</h1>
+            </div>
+        );
+    }
+}
